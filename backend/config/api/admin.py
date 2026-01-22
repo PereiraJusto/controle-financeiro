c6cc5742be2ee3b3b685b4a0aa02d855fcd154ac
PereiraJusto <rafa.spereira@icloud.com>
2026-01-22 16:26:48 -0300

    Continuação da Fase 2 - falta finalizar etapa Categoria

--- backend/config/api/admin.py
+++ backend/config/api/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Pessoa
+from .models import Pessoa, Categoria
+
 
 @admin.register(Pessoa)
 class PessoaAdmin(admin.ModelAdmin):
-    list_display = ("nome", "ativo", "criado_em")
+    list_display = ("nome", "ativo", "criado_em", "atualizado_em")
+    list_filter = ("ativo",)
+    search_fields = ("nome",)
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "ativo", "criado_em", "atualizado_em")
     list_filter = ("ativo",)
     search_fields = ("nome",)
--- backend/config/api/models.py
+++ backend/config/api/models.py
@@ -4,6 +4,17 @@ class Pessoa(models.Model):
     nome = models.CharField(max_length=150)
     ativo = models.BooleanField(default=True)
     criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+    
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    ativo = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
